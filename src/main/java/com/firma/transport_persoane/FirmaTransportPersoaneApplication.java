5832f547302c8790b0b4e1404c53018396a88db8
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-16 10:31:17 +0200

    Initial commit

---
+++ src/main/java/com/firma/transport_persoane/FirmaTransportPersoaneApplication.java
@@ -0,0 +1,13 @@
+package com.firma.transport_persoane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmaTransportPersoaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmaTransportPersoaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firma/transport_persoane/FirmaTransportPersoaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firma.transport_persoane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmaTransportPersoaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
