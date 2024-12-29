f669d49d9ec28ee3ed093c6ad1172a51d84921c8
Ruben <174733720+ruben-23@users.noreply.github.com>
2024-12-29 07:19:15 +0200

    Modificare app si navbar pentru a putea schimba optiunile intre pasageri si staff

--- src/frontend/firma-transport/src/App.jsx
+++ src/frontend/firma-transport/src/App.jsx
@@ -4,21 +4,43 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavbarFirma from './components/Navbar.jsx'
 import OrarRute from './components/OrarRute.jsx';
 import Feedback from './components/Feedback.jsx';
+import {useState} from "react";
+import InformatiiFirma from "./components/InformatiiFirma.jsx";
+import GestionareOrareRute from "./components/GestionareOrareRute.jsx";
 
 function App() {
+  const [rolUser, setRolUser] = useState('pasager');
+
+  const schimbaInPersonalFirma = () => {
+    setRolUser('staff');
+  };
+
+  const schimbaInPasageri = () => {
+    setRolUser('pasager');
+  };
+
 
   return (
-    <>
+
       <BrowserRouter>
-        <NavbarFirma />
-        <Routes>
-          <Route path="/" element={<OrarRute />} />
-          <Route path="/orare" element={<OrarRute />} />
-          <Route path="/feedback" element={<Feedback />} />
-        </Routes>
+        <NavbarFirma
+            rolUser={rolUser}
+            schimbaInPersonalFirma={schimbaInPersonalFirma}
+            schimbaInPasageri={schimbaInPasageri}
+        />
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<InformatiiFirma/>}/>
+            <Route path="/orare" element={<OrarRute/>}/>
+            <Route path="/feedback" element={<Feedback/>}/>
+            <Route path="/gestionare-orare" element={<GestionareOrareRute/>}/>
+            {/*<Route path="/gestionare-autobuze" element={<GestionareAutobuze />} />*/}
+            {/*<Route path="/rapoarte" element={<Rapoarte />} />*/}
+          </Routes></div>
       </BrowserRouter>
-    </>
-  );
+
+)
+  ;
 }
 
 export default App
--- src/frontend/firma-transport/src/components/Navbar.jsx
+++ src/frontend/firma-transport/src/components/Navbar.jsx
@@ -1,33 +1,94 @@
 
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
+import PropTypes from "prop-types";
 
-const NavbarFirma = () => {
+const NavbarFirma = ({ rolUser, schimbaInPersonalFirma, schimbaInPasageri }) => {
+  const navigate = useNavigate();
+
+  const handleSchimbareOptiuni  = () => {
+    if (rolUser === 'staff') {
+      navigate('/');
+      schimbaInPasageri();
+    } else {
+      navigate('/');
+      schimbaInPersonalFirma();
+    }
+  };
+
     return (
-      <div>
-        <header>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="/">
-              Firma Transport Persoane
-            </a>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/orare">
-                    Orare si Rute
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink className="nav-link" to="/feedback">
-                    Feedback
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </header>
-      </div>
+        <div>
+          <header>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+              <NavLink className="navbar-brand" to="/">
+                Firma Transport Persoane
+              </NavLink>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+
+                  {/*afisare butoane diferite in bara de navigare in functie de rolul userului*/}
+                  {rolUser === 'pasager' ? (
+                      <>
+                        <li className="nav-item">
+                          <NavLink className="nav-link" to="/orare">
+                            Orare si Rute
+                          </NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className="nav-link" to="/feedback">
+                            Feedback
+                          </NavLink>
+                        </li>
+                      </>
+                  ) : (
+                      <>
+                        <li className="nav-item">
+                          <NavLink className="nav-link" to="/gestionare-orare">
+                            Gestionare Orare si Rute
+                          </NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className="nav-link" to="/gestionare-autobuze">
+                            Gestionare Autobuze
+                          </NavLink>
+                        </li>
+                        <li className="nav-item">
+                          <NavLink className="nav-link" to="/rapoarte">
+                            Rapoarte
+                          </NavLink>
+                        </li>
+                      </>
+                  )}
+                </ul>
+
+               {/*buton pentru a schimba optiunile din bara de navigare */}
+                <ul className="navbar-nav ms-auto">
+                  {rolUser === 'pasager' ? (
+                      <li className="nav-item">
+                        <button className="btn btn-link nav-link" onClick={handleSchimbareOptiuni}>
+                          Mergi la Personal Firma
+                        </button>
+                      </li>
+                  ) : (
+                      <li className="nav-item">
+                        <button className="btn btn-link nav-link" onClick={handleSchimbareOptiuni}>
+                          Mergi la Pasageri
+                        </button>
+                      </li>
+                  )}
+                </ul>
+              </div>
+            </nav>
+          </header>
+        </div>
+
     );
-} 
+}
+
+NavbarFirma.propTypes = {
+  rolUser:  PropTypes.string.isRequired,
+  schimbaInPersonalFirma: PropTypes.func.isRequired,
+  schimbaInPasageri: PropTypes.func.isRequired,
+};
 
 export default NavbarFirma;
